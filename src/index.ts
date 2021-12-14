84d573cd9e0f0acd9f013724569b8d831c726f52
rquanx <runquant@gmail.com>
2021-12-15 03:09:01 +0800

    update: itaro入口类型修正

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { AppConfig } from "@tarojs/taro"
 import { ITARO_ENV } from "./constants"
 
-const itaro = (appConfig: any, cb?: (config) => any) => {
+const itaro = <T extends AppConfig>(appConfig: T, cb?: (config: T) => T) => {
   if (process.env[ITARO_ENV]) {
     const config = JSON.parse(process.env[ITARO_ENV] || "")
     return cb?.(config) ?? config
