b7fad5b7ca610d55b896230e7c46802c7f988670
rquanx <runquant@gmail.com>
2021-12-21 01:33:39 +0800

    feat: 优化itaro类型定义，提供更好的提示

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { AppConfig } from "@tarojs/taro"
 import { ITARO_ENV } from "./constants"
 
-const itaro = <T extends AppConfig>(appConfig: T, cb?: (config: T) => T) => {
+const itaro = <T extends AppConfig>(
+  appConfig: T,
+  cb?: (config: AppConfig & T) => AppConfig & T
+) => {
   if (process.env[ITARO_ENV]) {
     const config = JSON.parse(process.env[ITARO_ENV] || "")
     return cb?.(config) ?? config
