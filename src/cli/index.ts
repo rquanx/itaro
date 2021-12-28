ca1f71936d358353aea24fd16989ac4bab63b472
rquanx <runquant@gmail.com>
2021-12-29 01:40:56 +0800

    feat: workspace + type作为缓存区分

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,6 @@
 import { Command } from "commander"
+import * as md5 from 'crypto-js/md5';
+
 
 const program = new Command()
 program
@@ -12,6 +14,7 @@ program
 program.parse(process.argv)
 const options = program.opts()
 global.itaroWorkspace = options.path
+options.pad = md5(`${options.type ?? ''}${options.path ?? ''}`).toString()
 
 /* eslint-disable */
 // 必须使用require，动态加载，由于路径变量都是作为常量定义，所以在加载前需要预设好itaroWorkspace
